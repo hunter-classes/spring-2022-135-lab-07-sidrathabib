9c561ec42a7813730e27001c45d7509be85770e0
Henry Fan <henry.fan2313@gmail.com>
2022-04-21 20:50:03 -0400

    modified pretty-printing function for efficiency

--- pretty-printing.cpp
+++ pretty-printing.cpp
@@ -8,10 +8,8 @@
 std::string removeLeadingSpaces(std::string line) {
     std::string result;
     int i = 0;
-    if(isspace(line[i])) {
-        while (isspace(line[i])) {
-            i++;
-        }
+    while (isspace(line[i])) {
+      i++;
     }
     result = line.substr(i);
     return result;
